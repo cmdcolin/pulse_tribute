7378e8c77fbd63d6bc0577944dd3c42053938f12
Colin <colin.diesh@gmail.com>
2016-06-12 19:53:53 -0500

    Rebase drawing

--- index.js
+++ index.js
@@ -11,32 +11,47 @@ function svgr(id, w, h) {
 
 function plotData(data, id, w, h) {
     var svg = svgr(id, w, h);
-    var x;
-    var y;
-
-    x = d3.scale.linear().range([0, w]).domain(d3.extent(data, (d) => d.x1));
-    y = d3.scale.linear().range([h, 0]).domain(d3.extent(data, (d) => d.y1));
+    var x = d3.scale.linear().range([0, w]).domain(d3.extent(data, (d) => d.x1));
+    var y = d3.scale.linear().range([h, 0]).domain(d3.extent(data, (d) => d.y1));
 
     var valueline = d3.svg.line()
         .interpolate('linear')
         .y((d) =>  y(d.y1))
         .x((d) =>  x(d.x1));
 
-    var path = svg.append('path')
+    svg.selectAll("path")
+        .data(data)
+        .enter().append('path')
         .attr('d', valueline(data))
+        .style('fill', function(d) {console.log(d); return '#fff'; })
         .attr('stroke-width', '4')
-        .attr('fill', 'none');
+        .style('stroke', function(d) { return 'blue'; })
+    .selectAll("stop")
+      .data([
+        {offset: "0%", color: "steelblue"},
+        {offset: "50%", color: "gray"},
+        {offset: "100%", color: "red"}
+      ])
+    .enter().append("stop")
+      .attr("offset", function(d) { console.log('h2',d);return d.offset; })
+      .attr("stop-color", function(d) { return d.color; })
 
-
-    var totalLength = path.node().getTotalLength();
-    path
-        .attr('stroke-dasharray', `${totalLength} ${totalLength}`)
-        .attr('stroke-dashoffset', totalLength)
-        .attr('stroke', function(d) { console.log(d); return 'hsl(70,50%,50%)'; })
-        .transition()
-        .duration(2000)
-        .ease('linear')
-        .attr('stroke-dashoffset', 0);
+//
+//    var path = svg.append('path')
+//        .data(data)
+//        .attr('d', valueline(data))
+//        .attr('stroke-width', '4')
+//        .attr('fill', 'none');
+//
+//    path.attr('stroke-dasharray', `${path.node().getTotalLength()} ${path.node().getTotalLength()}`)
+//        .attr('stroke-dashoffset', path.node().getTotalLength())
+//        .transition()
+//        .duration(2000)
+//        .ease('linear')
+//        .attr('stroke', function(da) { console.log('hello2',da); return 'hsl(70,50%,50%)'; })
+//        .attr('stroke-dashoffset', 0);
+//    path2 = svg.selectAll('path');
+//    path2.attr('stroke', function(r) { console.log(r); return 'black'; });
 }
 
 function setupGraph(id, w, h) {
